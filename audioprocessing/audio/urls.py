fd65bb324bc7ad157e15242e645b7e804da3f3df
Pranjal Jain <61983132+pranjal-jain-coder@users.noreply.github.com>
2024-11-17 14:45:25 +0530

    Delete audioprocessing directory

--- audioprocessing/audio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.record_audio, name='record_audio'),
-    # path('', views.process_audio, name='process_audio'),
-    path('process/', views.process_audio, name='process_audio'),
-    path('map/',views.maps_audio,name="maps")
-]
